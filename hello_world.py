f5f4b6fa2dadbf7e0f6cf8951795fcb8a3db85d6
Jonathan Darrer <jdarrer@findmypast.com>
2024-11-11 19:34:14 +0000

    ✨ Escaping and multi-line text literals
    Co-authored-by: ForgottenFighter

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,7 @@
 print("Hello, World!")
+print() # a blank line
+print('text', "and more text")
+print("Suppose two swallows \"flock\" together")
+print('African or "European" swallows?')
+print("""For text on multiple lines,
+use triple quotes""")
